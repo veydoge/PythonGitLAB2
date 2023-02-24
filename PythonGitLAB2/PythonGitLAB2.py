4fe16968935c0def77f867c7ec1bc8ec054d1b4b
veydoge <veydoge@gmail.com>
2023-02-25 00:23:02 +0300

    Тоже самое но с функцией

--- PythonGitLAB2/PythonGitLAB2.py
+++ PythonGitLAB2/PythonGitLAB2.py
@@ -1,3 +1,7 @@
 import random
-mylist = [random.randint(5, 8 * 100) for i in range(18)]
+
+def generate_list(n):
+    return [random.randint(5, 8 * 100) for i in range(n)]
+
+mylist = generate_list(18)
 print(mylist)
